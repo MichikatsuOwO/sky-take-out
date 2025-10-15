583346c9fd72e80fb3e6892bfbc7c42fdb3d3e9a
LoveAnneLister <2974728131@qq.com>
2025-10-15 22:20:14 +0800

    feat(sky-take-out): day2-新增员工操作
    [详细说明]
    - 新增员工
    - 密码保存方式为md5

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -5,6 +5,8 @@ import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import com.sky.constant.MessageConstant;
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -24,4 +26,15 @@ public class GlobalExceptionHandler {
         return Result.error(ex.getMessage());
     }
 
+    @ExceptionHandler
+    public Result doSqlExceptionHandler(SQLIntegrityConstraintViolationException ex){
+        log.error("异常信息：{}", ex.getMessage());
+        String message = ex.getMessage();
+        if (message.contains("Duplicate")) {
+            String spilic[] = message.split(" ");
+            String msg = spilic[2];
+            return Result.error(msg + MessageConstant.USER_DUPLICATE);
+        }
+        return Result.error("用户名已存在");
+    }
 }
